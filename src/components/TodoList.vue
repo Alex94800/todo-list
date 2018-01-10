755ea0b2834bcb7af054e94df16fe4f443154f40
alexandre guedj <alex@alexandres-mbp.lan>
2018-01-10 19:03:04 +0100

    second commit

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,63 +1,107 @@
 <template>
   <div>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 text-center">
-          <h1>Todo List</h1>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand">Todo List</a>
         </div>
+        <ul class="nav navbar-nav">
+          <li><a href="#" @click="viewArchive = false">Liste actuelle</a></li>
+          <li><a href="#" @click="viewArchive = true">Taches archivées</a></li>
+        </ul>
       </div>
-      <div class="row">
+    </nav>
+
+    <div class="container">
+
+      <div class="row" v-if="!viewArchive">
         <div class="col-sm-6 col-sm-offset-2">
           <input class="form-control" type="text" placeholder="Ajouter tache" v-model="newTodo" @keyup.enter="addTodo()">
         </div>
-        <div class="col-md-2">
+        <div class="col-md-4">
           <button class="btn btn-primary" @click="addTodo()">Ajouter</button>
+          </button>
         </div>
       </div>
-      <div class="col-sm-10 col-sm-offset-1 text-center">
-        <br>
-        <table class="table">
-          <thead>
-            <tr>
-              <th class="text-center">Tache</th>
-              <th class="text-center">Fait</th>
-              <th class="text-center">Modifier</th>
-               <th class="text-center">Supprimer</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="todo in todos">
-              <td>{{todo.name}}</td>
-              <td>
-                <div class="form-check">
-                  <input class="form-check-input" type="checkbox" v-model="todo.isDone">
-                </div>
-              </td>
-              <td>
-                <button class="btn btn-xs btn-primary" @click="modifyTodo(todo)">
-                  <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>           
-                </button>
-              </td>
-              <td>
-                <button class="btn btn-xs btn-danger" @click="destroyTodo(todo)">
-                  <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>           
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+
+      <div class="row" v-if="!viewArchive">
+        <div class="col-sm-10 col-sm-offset-1 text-center">
+          <br>
+          <table class="table">
+            <thead>
+              <tr>
+                <th class="text-center">Tache</th>
+                <th class="text-center">Fait</th>
+                <th class="text-center">Modifier</th>
+                <th class="text-center">Supprimer</th>
+                <th class="text-center">Archiver</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="todo in todos">
+                <td>{{todo.name}}</td>
+                <td>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" v-model="todo.isDone">
+                  </div>
+                </td>
+                <td>
+                  <button class="btn btn-xs btn-primary" @click="modifyTodo(todo)">
+                    <span class="glyphicon glyphicon-edit" aria-hidden="true"></span>           
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-xs btn-danger" @click="destroyTodo(todo)">
+                    <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>           
+                  </button>
+                </td>
+                <td>
+                  <button class="btn btn-xs btn-warning" @click="archiveTodo(todo)">
+                    <span class="glyphicon glyphicon-download" aria-hidden="true"></span>           
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <div class="row" v-if="viewArchive">
+        <div class="col-xs-10 col-xs-offset-1 text-center">
+          <br>
+          <table class="table">
+            <thead>
+              <tr>
+                <th class="text-center"><h2>Taches archivées</h2></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="archivedTodo in archivedTodos">
+                <td>{{archivedTodo.name}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
       </div>
     </div>
   </div>
 </template>
+<style>
+.container{
+  margin-top: 80px;
+}
+</style>
 
 <script>
 export default {
   data () {
     return {
       todos: [],
+      archivedTodos: [],
       newTodo: "",
-      modifiedName:"Modifié!!"
+      viewArchive: false
     }
   },
 
@@ -83,6 +127,16 @@ export default {
 
     modifyTodo(todo){
       todo.name = prompt("Entrez un nouveau nom pour la tache")
+    },
+
+    archiveTodo(todo){
+      if(!todo.isDone){
+        window.alert("Impossible d'archiver une tache en cours.")
+      }
+      else{
+        this.archivedTodos.push(todo)
+        this.todos.splice(this.todos.indexOf(todo), 1)
+      }
     }
   }
 }
